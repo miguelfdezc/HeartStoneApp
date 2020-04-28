75284f259b93fc0d116ee99f551d8f81bea49afd
Miguel Fernández Carratalá <miguelfdezc99@gmail.com>
2020-04-28 17:55:08 +0200

    Implementado Ionic Taller 3.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  miForm: any;
+  login = false;
+
+  constructor(private fb: FormBuilder) {
+    this.miForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  doLogin() {
+    if(this.miForm.dirty && this.miForm.valid) {
+      console.log("Email: " + this.miForm.value.email);
+      console.log("Password: " + this.miForm.value.password);
+      this.login = true;
+    }
+  }
+}
